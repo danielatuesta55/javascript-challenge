93583a2239d9b35156cdd1925d5e8b4ee97b8ec5
DANIEL <daniel@la-MacBook-Pro-de-Macintosh.local>
2021-05-21 12:10:35 -0500

    Added initial files to repo

---
+++ StarterCode/static/js/app_helper.js
@@ -0,0 +1,46 @@
+//starter file from TA - Erin Wills
+// provided comments for basic version of HW
+// included function names and filteredData variable
+
+// from data.js
+const tableData = data;
+
+// get table references where table will be inserted
+
+
+function buildTable(data) {
+  // When the page loads, it needs to display the table
+  // But if the table reloads then you may need to ensure the 
+  // previous output is cleared/overwritten from scratch 
+
+  // Think of the class activities for generating tables
+
+}
+
+function handleClick() {
+
+  // Grab the datetime value from the filter
+  
+
+  // grab all the table data and set to filteredData
+  
+  // Check to see if a date was entered and filter the
+  // data using that date.
+
+
+  
+
+  
+
+  // Rebuild the table using the filtered data
+  // @NOTE: If no date was entered, then filteredData will
+  // just be the original tableData.
+  // take your filtered data and put it into the buildTable to rebuild the table with the filtered data
+  buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button that calls handleClick on a click of the filter button
+
+
+// Build the table when the page loads
+buildTable(tableData);
